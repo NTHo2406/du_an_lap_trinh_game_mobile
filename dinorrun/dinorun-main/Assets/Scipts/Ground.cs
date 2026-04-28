e9235f38997b87a9905895dcbc612a40f3ff8016
NTHo2406 <nth24042006@gmail.com>
2026-04-28 12:56:14 +0700

    laptrinhgame

---
+++ dinorrun/dinorun-main/Assets/Scipts/Ground.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Ground : MonoBehaviour
+{
+    public float speed = 5f;
+    public float width = 17.78f;
+
+    public Text scoreText;
+    private int score = 0;
+
+    private bool isGameOver = false;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(AddScore), 1f, 1f);
+    }
+
+    void Update()
+    {
+        if (isGameOver) return;
+
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (transform.position.x <= -width)
+        {
+            transform.position += new Vector3(width * 2f, 0, 0);
+        }
+    }
+
+    void AddScore()
+    {
+        if (isGameOver) return;
+
+        score++;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void StopGame()
+    {
+        isGameOver = true;
+        CancelInvoke();
+    }
+}
---
+++ dinorrun/dinorun-main/Assets/Scipts/ObstacleMove.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ObstacleMove : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        if (transform.position.x < -15f)
+            Destroy(gameObject);
+    }
+}
